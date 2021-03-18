9f10386b4bd0b9fd32b92d730ad8a43c7b60327c
Kadorve <protriarii@mail.ru>
2021-03-18 23:31:38 +0500

    7. Date. Отображение даты. [#472572]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI extends Item {
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = getCreated().format(formatter);
+        System.out.println(currentDateTimeFormat);
+    }
+}
