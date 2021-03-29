a50a95f5a43665f04b42bd8b14c92c0b9721a1da
Kadorve <protriarii@mail.ru>
2021-03-29 15:27:35 +0500

    9. toString [#472584]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -29,7 +29,7 @@ public class Item {
     public String toString() {
         return "Item{" +
                 "id=" + id +
+                ", name='" + name + '\'' +
                 '}';
     }
-
 }
