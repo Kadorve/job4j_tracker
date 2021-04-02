8e1b301863bf09944fe2d234199472cc5aa1e593
Kadorve <protriarii@mail.ru>
2021-04-02 11:44:36 +0500

    5.1. Тестирование. Подготовка данных. [#472514]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 public class StartUITest {
@@ -16,5 +16,30 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker(); //создаем объект tracker.
+        Item item = new Item("new item"); //создаем объект item.
+        tracker.add(item); //добавляем item в tracker. После этой операции возникает id.
+        String[] answers = {String.valueOf(item.getId()), //Достаем item.id и создаем массив с ответами пользователя.
+        "replaced item"};
+        StartUI.replaceItem(new StubInput(answers), tracker); //Вызываем тестируемый метод replaceItem. Это действие изменит состояние объекта tracker.
+        Item replaced = tracker.findById(item.getId()); //Поиск по item.id замененного item в объекте tracker.
+        assertThat(replaced.getName(), is("replaced item")); // Сравниаем имя найденной заявки с ожидаемой.
+    }
+
+    @Test
+    public void deleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()),
+        "removed item"};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertThat(deleted, is(nullValue()));
+
     }
 }
